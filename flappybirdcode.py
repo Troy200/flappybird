c9a323b6ded744da724ff84e7c11c6d04a069ecc
Troy200 <kuriyanthomas27@gmail.com>
2025-08-27 17:57:33 +0200

    Add files via upload

--- flappybirdcode.py
+++ flappybirdcode.py
@@ -5,8 +5,9 @@ pygame.init()
 WIDTH=800
 HEIGHT=600
 
-fps=50
+fps=60
 x=0
+gameover=False
 
 screen= pygame.display.set_mode((WIDTH,HEIGHT))
 bg=pygame.image.load("pro game devloper/flappy bird/images/bg.png")
@@ -24,11 +25,32 @@ class bird(pygame.sprite.Sprite):
         self.image=self.images[self.index]
         self.rect=self.image.get_rect()
         self.rect.center=[x,y]
+        self.counter=0
+        self.velocity=0
+        self.click=False
     def update(self):
-        self.index+=1
-        if self.index>2:
-            self.index=0
-        self.image=self.images[self.index]
+        global gameover
+        if gameover==False:
+            self.counter+=1
+            if self.counter>5:
+                self.counter=0
+                self.index+=1
+                if self.index>2:
+                    self.index=0
+                self.image=self.images[self.index]
+            
+
+        self.velocity+=0.2
+        self.rect.y+=self.velocity
+        if self.rect.y>480:
+            self.rect.y=480
+            gameover=True
+        if pygame.mouse.get_pressed()[0]==1 and self.click==False:  
+            self.velocity= -5
+            self.click=True
+        if pygame.mouse.get_pressed()[0]==0:
+            self.click=False
+
 
 
 bird_group=pygame.sprite.Group()
@@ -46,9 +68,10 @@ while run:
     clock.tick(fps)
     screen.blit(bg,(0,0))
     screen.blit(ground,(x,500))
-    x-=10
-    if x<-100:
-        x=0
+    if gameover==False:
+        x-=3
+        if x<-100:
+            x=0
 
     bird_group.draw(screen)
     bird_group.update()
